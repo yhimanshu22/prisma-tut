95f7611d01403b9a6f80d381e484982e9e936863
HIMANSHU YADAV <yhimanshu22@iitk.ac.in>
2024-05-17 23:30:40 +0530

    added CRUD operations

--- script.ts
+++ script.ts
@@ -4,11 +4,54 @@ const prisma = new PrismaClient()
 
 async function main() {
     //creating a user-------->
+
     // const userCreate = await prisma.user.create({ data: { name: 'himu', username: 'fhb', email: 'sddc@gmail.com' } })
     //console.log(userCreate);
+
     //find all user------>
+
     const findAllUsers = await prisma.user.findMany()
     console.log(findAllUsers);
+
+    //retrive a single record 
+
+    const newUser = await prisma.user.create({
+        data: {
+            name: 'John Doe',
+            username: 'johndoe',
+            email: 'johndoe@example.com',
+            isAdmin: false,
+            preferences: { theme: 'dark' },
+        },
+    });
+    console.log(newUser);
+
+    //update a record->
+    const updatedUser = await prisma.user.update({
+        where: { id: 134 },
+        data: { email: 'hium@gmail.com' },
+
+    })
+
+    //delete a user-------->
+    const deletedUser = await prisma.user.delete({
+        where: { id: 123 },
+    })
+
+    //create a new record or update an existing record-------->
+    const upsertedUser = await prisma.user.upsert({
+        where: { email: 'humf' },
+        update: { name: 'sdf' },
+        create: {
+            name: 'sdf',
+            username: 'wer',
+            email: 'werdfg',
+            isAdmin: false,
+            preferences: { theme: 'dark' },
+        }
+    })
+
+
 }
 
 main()
